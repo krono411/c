1bc4a84436ba3992f23811950d356d9d27976ba0
krono411 <313041@s.lhvs.tyc.edu.tw>
2024-12-19 15:25:38 +0800

    Update 20241219建立類別.cpp

--- 20241219建立類別.cpp
+++ 20241219建立類別.cpp
@@ -9,6 +9,18 @@ class myTime{
 		
 };
 
+void myTime::printTime(){
+    cout<<hour<<";"<<minute<<"\n";
+}
 int main(){
+	myTime now, open;
+	now.hour=8;
+	now.minute=18;
+	cout<<"現在時間:"
+        now.printTime();
+	open.hore =9;
+	open.minute =10;
+        cout<<"開門時間:";
+        open.printTime();
 	
 } 
